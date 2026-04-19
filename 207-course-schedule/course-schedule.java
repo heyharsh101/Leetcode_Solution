746d2b7998c67a2d0083529d2968909ac4328394
Harsh Deep <161753130+heyharsh101@users.noreply.github.com>
2026-04-19 15:17:04 +0530

    Time: 7 ms (60.38%) | Memory: 47.1 MB (50.76%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -4,30 +4,27 @@ class Solution {
         for(int i=0; i<numCourses; i++){
             adj.add(new ArrayList<>());
         }
+        int[] degree = new int[numCourses];
         for(int[] pre: prerequisites){
             adj.get(pre[1]).add(pre[0]);
+            degree[pre[0]]++;
         }
-        boolean[] vis = new boolean[numCourses];
-        boolean[] path = new boolean[numCourses];
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0 ; i<numCourses; i++){
+            if(degree[i] == 0) q.add(i);
+        }
 
-        for(int i=0; i<numCourses; i++){
-            if(!vis[i] && dfs(i, adj, vis, path)) {
-                return false;
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            cnt++;
+
+            for(int next : adj.get(node)){
+                degree[next]--;
+                if(degree[next] == 0) q.add(next);
             }
         }
-        return true;
-    }
-    public boolean dfs(int node, List<List<Integer>> adj, boolean[] vis, boolean[] path){
-        vis[node] = path[node] = true;
-
-        for(int next : adj.get(node)){
-            if(!vis[next] && dfs(next, adj, vis, path)){
-                return true;
-            } else if(path[next]){
-                return true;
-            }
-        }
-        path[node] = false;
-        return false;
+        return cnt == numCourses;
     }
 }
