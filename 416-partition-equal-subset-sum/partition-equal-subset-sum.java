88436db6bb223ae119dcba2d5a6552e6c58b22ae
Harsh Deep <161753130+heyharsh101@users.noreply.github.com>
2026-04-30 10:48:40 +0530

    Time: 110 ms (10.02%) | Memory: 72.7 MB (28.48%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,21 +1,32 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int total = 0;
-        for(int num : nums){
-            total +=num;
+        int n = nums.length;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum = sum +nums[i];
         }
+        if(sum % 2 != 0) return false;
 
-        if(total % 2 != 0) return false;
-        int targetSum = total / 2;
-        boolean[] dp = new boolean[targetSum +1];
-    
-        dp[0] = true;
-        for(int num : nums){
-            for(int i = targetSum; i>= num; i--){
-                dp[i] = dp[i] || dp[i - num];
-    
-            }
+        sum = sum / 2;
+
+        Boolean[][] dp = new Boolean[n][sum+1];
+        
+        return Memorization(0, sum, nums, dp);
+
+    }
+    boolean Memorization(int i, int sum, int[] nums, 
+    Boolean[][] dp){
+        if(sum == 0) return true;
+        if(i >= nums.length) return false;
+
+        if(dp[i][sum] != null) return dp[i][sum];
+
+        boolean take = false;
+        if(nums[i] <= sum){
+            take = Memorization(i+1, sum - nums[i], nums, dp);
         }
-        return dp[targetSum];
+        boolean nottake = Memorization(i+1, sum, nums, dp);
+
+        return dp[i][sum] = take || nottake;
     }
 }
