2bc4e1ce7055652f2d4fcd8f96df5baf7b81ae25
Harsh Deep <161753130+heyharsh101@users.noreply.github.com>
2026-04-30 10:54:34 +0530

    Time: 68 ms (34.47%) | Memory: 47.5 MB (54.83%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -9,24 +9,21 @@ class Solution {
 
         sum = sum / 2;
 
-        Boolean[][] dp = new Boolean[n][sum+1];
-        
-        return Memorization(0, sum, nums, dp);
-
-    }
-    boolean Memorization(int i, int sum, int[] nums, 
-    Boolean[][] dp){
-        if(sum == 0) return true;
-        if(i >= nums.length) return false;
-
-        if(dp[i][sum] != null) return dp[i][sum];
-
-        boolean take = false;
-        if(nums[i] <= sum){
-            take = Memorization(i+1, sum - nums[i], nums, dp);
+        boolean[][] dp = new boolean[n+1][sum+1];
+        for(int i=0; i<=n; i++){
+            dp[i][0] = true;
         }
-        boolean nottake = Memorization(i+1, sum, nums, dp);
+        for(int i=1; i<=n ;i++){
+            for(int j=1; j<=sum; j++){
+                dp[i][j] = dp[i-1][j];
+                
+                if(nums[i-1] <= j){
+                    dp[i][j] = dp[i][j] || dp[i-1][j - nums[i-1]];
+                }
+                
+            }
+        }
+        return dp[n][sum];
 
-        return dp[i][sum] = take || nottake;
     }
 }
