5f5e0183d02c1466715f8366f7f359019fad6294
Harsh Deep <161753130+heyharsh101@users.noreply.github.com>
2026-05-20 14:50:04 +0530

    Time: 30 ms (13.78%) | Memory: 53.9 MB (50.20%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,20 +1,25 @@
 class Solution {
+    int[][] dp;
+    int n, m;
     public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
-        int[][] dp = new int[n+1][m+1];
-        
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(text1.charAt(i-1) == text2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else {
-                    int ans1 = dp[i-1][j];
-                    int ans2 = dp[i][j-1];
-                    dp[i][j] = Math.max(ans1, ans2);
-                }
-            }
+        n = text1.length();
+        m = text2.length();
+        dp = new int[n+1][m+1];
+        for(int i=0; i<=n; i++){
+            Arrays.fill(dp[i], -1);
         }
-        return dp[n][m];
+        return Memorization(text1, text2, 0, 0);
+    }
+    int Memorization(String t1, String t2, int i, int j){
+        if(i==n || j==m) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(t1.charAt(i) == t2.charAt(j)){
+            dp[i][j] = 1+Memorization(t1, t2, i+1, j+1);
+        } else {
+            dp[i][j] = Math.max(Memorization(t1, t2, i+1, j), Memorization(t1, t2, i, j+1));
+        }
+        return dp[i][j];
     }
 }
