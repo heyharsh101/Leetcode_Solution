2a60568d01684af231faf82ddd7f1939a4226f10
Harsh Deep <161753130+heyharsh101@users.noreply.github.com>
2026-08-09 15:10:48 +0530

    Time: 4 ms (18.61%) | Memory: 82.8 MB (98.39%) - LeetSync

---
+++ 2591-frog-jump-ii/frog-jump-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxJump(int[] stones) {
+        int n = stones.length;
+
+        int[] dp = new int[n];
+
+        dp[0] =0;
+        dp[1] = stones[1] - stones[0];
+        if(n==2){
+            return dp[1];
+        }
+
+        for(int i=2; i<n; i++){
+            dp[i] = Math.max(dp[i-1], stones[i] - stones[i-2]);
+        }
+        return dp[n-1];
+
+    }
+}
